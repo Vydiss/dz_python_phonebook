9dd57e11d9ef8eb42c694c7234a2a534b9a97cff
Endruman <hayllihuk@gmail.com>
2024-04-09 10:50:23 +0300

    add function copying_string

--- phonebook.py
+++ phonebook.py
@@ -21,19 +21,20 @@
 для изменения и удаления данных.
 """
 
-from csv import DictReader, DictWriter # Импортируем методы DictReader и DictWriter из модуля csv
+from csv import DictReader, DictWriter  # Импортируем методы DictReader и DictWriter из модуля csv
 from os.path import exists
 
 patronymic = 'Urievich'
-file_name = 'phonebook.csv'
 
 class LenNumberError(Exception):
     def __init__(self, txt):
         self.txt = txt
 
+
 class InvalidNameError(Exception):
     def __init__(self, txt):
         self.txt = txt
+
 
 #
 def get_info():
@@ -87,6 +88,79 @@ def get_info():
     return [first_name, last_name, phone_number]
 
 
-def file_app():
-    with open('phonebook.csv') as f_n:
-        pass
+def create_file(file_path):
+    # with - Менеджер контекста
+    with open(file_path, "w", encoding='utf-8') as data:
+        f_writer = DictWriter(data, fieldnames=['Имя', 'Фамилия', 'Телефон'])
+        f_writer.writeheader()
+
+
+def read_file(file_path):
+    with open(file_path, "r", encoding='utf-8') as data:
+        f_reader = DictReader(data)
+        return list(f_reader)
+
+
+def write_file(file_path, lst):
+    res = read_file(file_path)
+    for el in res:
+        if el["Телефон"] == str(lst[2]):
+            print("Такой телефон уже есть")
+            return
+
+    obj = {"Имя": lst[0], "Фамилия": lst[1], "Телефон": lst[2]}
+    res.append(obj)
+    with open(file_path, "w", encoding='utf-8', newline='') as data:
+        f_writer = DictWriter(data, fieldnames=['Имя', 'Фамилия', 'Телефон'])
+        f_writer.writeheader()
+        f_writer.writerows(res)
+
+
+def copying_string(file_path, my_str):
+    num_str = int(input('Введите номер строки для копирования: ')) - 1
+    if num_str > len(my_str) - 1 or num_str < 0:
+        print('Такой строки нет')
+
+    else:
+        my_str = my_str[num_str]
+        res = read_file(file_path)
+        for el in res:
+            if el["Телефон"] == my_str["Телефон"]:
+                print("Такая строка уже есть")
+                return
+
+        res.append(my_str)
+        with open(file_path, "w", encoding='utf-8', newline='') as data:
+            f_writer = DictWriter(data, fieldnames=['Имя', 'Фамилия', 'Телефон'])
+            f_writer.writeheader()
+            f_writer.writerows(res)
+
+
+file_name = 'phone.csv'
+file_name_2 = 'important_phone.csv'
+
+def main():
+    print('Команды: \n q-выход \n w-запись \n r-вывод \n c-копирование')
+    while True:
+        command = input("Введите команду: ")
+        if command == 'q':
+            break
+        elif command == 'w':
+            if not exists(file_name):
+                create_file(file_name)
+            write_file(file_name, get_info())
+        elif command == 'c':
+            if not exists(file_name_2):
+                create_file(file_name_2)
+            lst_2 = read_file(file_name)
+            copying_string(file_name_2, lst_2)
+        elif command == 'r':
+            if not exists(file_name):
+                print("Файл отсутствует. Создайте его")
+                continue
+            print(*read_file(file_name))
+            print('Новвый файл')
+            print(*read_file(file_name_2))
+
+
+main()
